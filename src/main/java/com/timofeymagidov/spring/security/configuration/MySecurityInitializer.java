5372afd6dfed0d53dd36a54b2c5668fb6c98faa7
1user1-1 <120933718+1user1-1@users.noreply.github.com>
2023-09-29 12:35:42 +0300

    App Configuration

---
+++ src/main/java/com/timofeymagidov/spring/security/configuration/MySecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.timofeymagidov.spring.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends
+        AbstractSecurityWebApplicationInitializer {
+}
