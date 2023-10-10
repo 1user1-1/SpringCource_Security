b3f262c482d7d60b4a5ea3807c82f64bf5c75b53
1user1-1 <120933718+1user1-1@users.noreply.github.com>
2023-10-10 10:45:44 +0300

    Authorization

--- src/main/java/com/timofeymagidov/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/timofeymagidov/spring/security/configuration/MySecurityConfig.java
@@ -1,6 +1,7 @@
 package com.timofeymagidov.spring.security.configuration;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -11,6 +12,16 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/")
+                .hasAnyRole("EMPLOYEE", "HR", "MANAGER")
+                .antMatchers("/hr_info").hasRole("HR")
+                .antMatchers("/manager_info/**").hasRole("MANAGER")
+                .and().formLogin().permitAll();
+    }
+
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         UserBuilder userBuilder = User.withDefaultPasswordEncoder();
 
@@ -19,7 +30,7 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
                         password("timofey").roles("EMPLOYEE"))
                 .withUser(userBuilder.username("elena").
                         password("elena").roles("HR"))
-                .withUser(userBuilder.username("ivam").
+                .withUser(userBuilder.username("ivan").
                         password("ivan").roles("MANAGER", "HR"));
 
     }
